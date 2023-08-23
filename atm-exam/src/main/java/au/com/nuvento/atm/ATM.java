fe2c1e1a26e3df5db57a3d0b52b81a9950db6486
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2023-08-23 14:27:36 +1000

    Finished Rough ATM

--- atm-exam/src/main/java/au/com/nuvento/atm/ATM.java
+++ atm-exam/src/main/java/au/com/nuvento/atm/ATM.java
@@ -2,13 +2,13 @@ package au.com.nuvento.atm;
 
 
 /**
- * Hello world!
+ *
  *
  */
 public class ATM
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
